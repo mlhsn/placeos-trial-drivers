9b0eb61333a2b437368f6a5f9f2eb71faf44a782
mlhsn <mahmudul.hasan@monstar-lab.com>
2023-02-02 16:20:26 +0600

    refactor: get data from request path

--- drivers/mlhsn/xkcd_spec.cr
+++ drivers/mlhsn/xkcd_spec.cr
@@ -7,7 +7,7 @@ DriverSpecs.mock_driver "Mlhsn::Xkcd" do
   )
 
   expect_http_request do |request, response|
-    if (/(\/[0-9]+\/)/ =~ request.url).nil?
+    if (/(\/[0-9]+\/)/ =~ request.path).nil?
       response.status_code = 200
       response << "{\"month\": \"2\", \"num\": 2732, \"link\": \"\", \"year\": \"2023\", \"news\": \"\", \"safe_title\": \"Bursa of Fabricius\", \"transcript\": \"\", \"alt\": \"If an anatomical structure is named for a person, it means they were the only person to have it. Pierre Paul Broca had a special area of his brain that created powerful magnetic fields, enabling him to do 19th century fMRI research.\", \"img\": \"https://imgs.xkcd.com/comics/bursa_of_fabricius.png\", \"title\": \"Bursa of Fabricius\", \"day\": \"1\"}"
     else
